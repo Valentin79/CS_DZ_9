a395beb1dbc1c43256234c94cdb52f777c7f0291
Valentin <99755643+Valentin79@users.noreply.github.com>
2023-11-26 22:25:46 +0300

    dz_9

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Text.Json;
+using System.Xml.Serialization;
+
+namespace CS_DZ_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var jsonFile = "{\"Current\":{\"Time\":\"2023-06-18T20:35:06.722127+04:00\"," +
+                "\"Temperature\":29,\"Weathercode\":1,\"Windspeed\":2.1,\"Winddirection\":1}," +
+                "\"History\":[{\"Time\":\"2023-06-17T20:35:06.77707+04:00\",\"Temperature\":29,\"Weathercode\":2,\"Windspeed\":2.4,\"Winddirection\":1}," +
+                "{\"Time\":\"2023-06-16T20:35:06.777081+04:00\",\"Temperature\":22,\"Weathercode\":2,\"Windspeed\":2.4,\"Winddirection\":1}," +
+                "{\"Time\":\"2023-06-15T20:35:06.777082+04:00\",\"Temperature\":21,\"Weathercode\":4,\"Windspeed\":2.2,\"Winddirection\":1}]}";
+
+
+            //Десериализуем json в объект класса
+            WeatherClass.WeatherInfo? weatherInfo = JsonSerializer.Deserialize<WeatherClass.WeatherInfo>(jsonFile);
+            Console.WriteLine(weatherInfo);
+            
+            //Сериализуем объект класса в xml
+            var serialazer = new XmlSerializer(typeof(WeatherClass.WeatherInfo));
+            serialazer.Serialize(Console.Out, weatherInfo);
+
+
+        }
+
+
+    }
+}/*-----------------Получился следующий код--------------->
+   <?xml version="1.0" encoding="Codepage - 866"?>
+<WeatherInfo xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+  <Current>
+    <Time>2023-06-18T19:35:06.722127+03:00</Time>
+    <Temperature>29</Temperature>
+    <Weathercode>1</Weathercode>
+    <Windspeed>2.1</Windspeed>
+    <Winddirection>1</Winddirection>
+  </Current>
+  <History>
+    <Weather>
+      <Time>2023-06-17T19:35:06.77707+03:00</Time>
+      <Temperature>29</Temperature>
+      <Weathercode>2</Weathercode>
+      <Windspeed>2.4</Windspeed>
+      <Winddirection>1</Winddirection>
+    </Weather>
+    <Weather>
+      <Time>2023-06-16T19:35:06.777081+03:00</Time>
+      <Temperature>22</Temperature>
+      <Weathercode>2</Weathercode>
+      <Windspeed>2.4</Windspeed>
+      <Winddirection>1</Winddirection>
+    </Weather>
+    <Weather>
+      <Time>2023-06-15T19:35:06.777082+03:00</Time>
+      <Temperature>21</Temperature>
+      <Weathercode>4</Weathercode>
+      <Windspeed>2.2</Windspeed>
+      <Winddirection>1</Winddirection>
+    </Weather>
+  </History>
+</WeatherInfo>
+  */
+        
---
+++ WeatherClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_DZ_9
+{
+    public class WeatherClass
+    {
+        public class Weather
+        {
+            public DateTime Time { get; set; }
+            public double Temperature { get; set; }
+            public int Weathercode { get; set; }
+            public double Windspeed { get; set; }
+            public int Winddirection { get; set; }
+        }
+
+        public class WeatherInfo
+        {
+            public Weather Current { get; set; }
+            public List<Weather> History { get; set; }
+
+            public override string ToString()
+            {
+                return $"{Current.Time}, {Current.Temperature},{Current.Weathercode} " +
+                    $"{Current.Windspeed}, {Current.Winddirection}";
+            }
+        }
+    }
+}
